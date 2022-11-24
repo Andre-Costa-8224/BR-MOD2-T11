4817bd0702b970a8ddcb05a5053ad5632b5af7a9
André <andrecosta8224@gmail.com>
2022-11-24 13:08:22 -0300

    bird random position added

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -1,10 +1,11 @@
 from dino_runner.components.obstacles.obstacle import Obstacle
 from dino_runner.utils.constants import BIRD
+from random import choice
 
 class Bird(Obstacle):
     def __init__(self):
         super().__init__(BIRD, 0)
-        self.rect.y = 250
+        self.rect.y = choice((250, 300, 360))
         self.index = 0
     
     def draw(self, screen):
